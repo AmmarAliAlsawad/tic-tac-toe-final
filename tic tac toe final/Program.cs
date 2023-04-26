4a3236631418dd17313259d3272ed85931df04e5
AmmarAliAlsawad <123181767+AmmarAliAlsawad@users.noreply.github.com>
2023-04-26 12:38:00 +0500

    Update Program.cs

--- tic tac toe final/Program.cs
+++ tic tac toe final/Program.cs
@@ -1,25 +1,26 @@
-﻿using System;
+using System;
 
-namespace tic_tac_toe_final
+namespace TicTacToe
 {
     internal class Program
     {
-        static int[] board = new int[9];
+        static int[] board = new int[9]; // initialize the game board
         static void Main(string[] args)
         {
+            // clear the game board
             for (int i = 0; i < 9; i++)
             {
                 board[i] = 0;
             }
-            
 
             int userTurn = -1;
             int computerTurn = -1;
             Random rand = new Random();
 
+            // start the game loop
             while (checkForWinner() == 0) {
                 
-                // don't allow the human to choose an already occupied square
+                // get user's turn
                 while (userTurn == -1 || board[userTurn] != 0)
                 {
                     Console.WriteLine("Please enter a number from 0 to 8");
@@ -28,7 +29,7 @@ namespace tic_tac_toe_final
                 }
                 board[userTurn] = 1;
 
-                // don't let the computer pick an invalid number
+                // get computer's turn
                 while((computerTurn == -1) || (board[computerTurn] != 0))
                 {
                     computerTurn = rand.Next(8);
@@ -38,100 +39,50 @@ namespace tic_tac_toe_final
                 }
 
                 board[computerTurn] = 2;
-                printBoard();
+                printBoard(); // print the updated game board
 
             }
-            Console.WriteLine("Player " + checkForWinner() + "won the game!");
-
-          
+            // announce the winner
+            Console.WriteLine("Player " + checkForWinner() + " won the game!");
 
         }
 
+        // check for a winner and return the player number or 0 if no winner
         private static int checkForWinner()
-        {
-            // return a 0 if nobody won. return the player number if they won.
-            
-            // top row.
-            if (board[0] == board[1] && board[1] == board[2])
-            {
-                return board[0]; 
+        {           
+            // check rows
+            for (int i = 0; i <= 6; i += 3) {
+                if (board[i] != 0 && board[i] == board[i + 1] && board[i + 1] == board[i + 2]) {
+                    return board[i];
+                }
             }
 
-            // second row.
-            if (board[3] == board[4] && board[4] == board[5])
-            {
-                return board[3];
+            // check columns
+            for (int i = 0; i <= 2; i++) {
+                if (board[i] != 0 && board[i] == board[i + 3] && board[i + 3] == board[i + 6]) {
+                    return board[i];
+                }
             }
 
-            // third row.
-            if (board[6] == board[7] && board[7] == board[8])
-            {
-                return board[6];
-            }
-
-            // first column.
-            if (board[0] == board[3] && board[3] == board[6])
-            {
+            // check diagonals
+            if (board[0] != 0 && board[0] == board[4] && board[4] == board[8]) {
                 return board[0];
             }
-
-            // second column.
-            if (board[1] == board[4] && board[4] == board[7])
-            {
-                return board[1];
-            }
-
-            // third column.
-            if (board[2] == board[5] && board[5] == board[8])
-            {
+            if (board[2] != 0 && board[2] == board[4] && board[4] == board[6]) {
                 return board[2];
             }
-
-            // first diagonal.
-            if (board[0] == board[4] && board[4] == board[8])
-            {
-                return board[0];
-            }
-
-            // second diagonal.
-            if (board[2] == board[4] && board[4] == board[6])
-            {
-                return board[2];
-            }
-
 
             return 0; 
         }
 
+        // print the game board
         private static void printBoard()
         {
-            for (int i = 0; i < 9; i++)
-            {
-                // print the board.
-                // Console.WriteLine("Square " + i + "contains" + board[i]);
-
-                // print x or o for each square
-                // 0 means unoccupied. 1 means player 1 (x) 2 means player 2 (o)
-
-                if (board[i] == 0)
-                {
-                    Console.Write(".");
-                }
-                if (board[i] == 1)
-                {
-                    Console.Write("x");
-                }
-                if (board[i] == 2)
-                {
-                    Console.Write("o");
-                }
-
-                // print new line every 3rd charater
-                if (i == 2 || i == 5 || i == 8)
-                {
-                    Console.WriteLine();
-                }
-            }
+            Console.WriteLine(" " + board[0] + " | " + board[1] + " | " + board[2] + " ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" " + board[3] + " | " + board[4] + " | " + board[5] + " ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine(" " + board[6] + " | " + board[7] + " | " + board[8] + " ");
         }
     }
 }
